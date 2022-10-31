a6bc7f3311e92a0d299a6ae1855036aac4b5d727
sayrish <sayrrish1@gmail.com>
2022-10-31 15:36:23 +0530

    Remove Word From Paragraph

---
+++ src/com/HashTable/BST/HashTable.java
@@ -0,0 +1,28 @@
+package com.HashTable.BST;
+
+public class HashTable {
+	  public void remove(String str ,String word){
+	        // split The String Using Split () method
+	        String [] msg = str.split(" ");
+	        String new_wrd = " ";
+	        // Iterating By using for each loop
+	        for(String words:msg){
+	            if(!words.equals(word)){
+	                new_wrd += words+" ";
+
+	            }
+	        }
+	        System.out.println(new_wrd);
+	    }
+	    public static void main(String[] args) {
+	        String str = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
+	        // Word Remove From String Word
+	      String word =  "avoidable";
+	      // calling Method By passing Both String
+	        HashTable tree = new HashTable();
+	
+	        
+	        tree.remove(str,word);
+	    }
+	
+}
